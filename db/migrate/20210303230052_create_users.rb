fd25698da54d2713c5618b3f498c58899ba220f9
Trayshawn <traytwebb14@yahoo.com>
2021-03-04 14:04:42 -0600

    added default account balance amount

--- db/migrate/20210303230052_create_users.rb
+++ db/migrate/20210303230052_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name 
-      t.integer :account_balance 
+      t.integer :account_balance, default: 200
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_230052) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "account_balance"
+    t.integer "account_balance", default: 200
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
