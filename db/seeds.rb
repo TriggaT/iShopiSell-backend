4b1c36b21ed7fad195560e136845e25f0de56df8
Trayshawn <traytwebb14@yahoo.com>
2021-03-04 15:59:45 -0600

    product seed data included

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@
 jeff = User.create(name: "Jeff", account_balance: "1000") 
 neisha = User.create(name: "Neisha", account_balance: "500")
 trey = User.create(name: "Trey", account_balance: "200")
+
+music = Product.create(name: "Music CDs", price: 15, quantity: 50, user_id: 3)
+makeup = Product.create(name: "Make-Up Kit", price: 20, quantity: 20, user_id: 2)
+book = Product.create(name: "The Best Thing Yet", price: 18, quantity: 50, user_id: 2)
+personal_assistant = Product.create(name: "Alexa", price: 50, quantity: 100, user_id:1 )
+phone = Product.create(name: "Limited Edition iPhone", price: 200, quantity: 1, user_id: 1)
+boardwalk = Product.create(name: "Boardwalk", price: 400, quantity: 10, user_id: 1) 
