6f310702275ca3d1b819365c4fb6ffeb557ba181
Trayshawn <traytwebb14@yahoo.com>
2021-03-04 16:10:54 -0600

    added instance and array to json methods to products

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+
+    def index 
+        products = Product.all 
+        render json: Product.array_to_json
+    end 
+
+    
+
+    
+
+
+
+
+   
+
+end 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,24 @@
 class Product < ApplicationRecord
     belongs_to :user
 
+    def self.array_to_json 
+        self.all.map do |p|
+            p.instance_to_json 
+        end 
+    end 
+
+    def instance_to_json
+        {
+            id: self.id,
+            price: self.price,
+            quantity: self.quantity,
+            user: {
+                id: self.user.id,
+                name: self.user.name
+            }           
+        }
+    end 
+
+
+
 end    
