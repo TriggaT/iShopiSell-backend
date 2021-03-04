5b17679c866613d3975df748e3fc1dc7b4af3049
Trayshawn <traytwebb14@yahoo.com>
2021-03-04 14:41:10 -0600

    added create and show method to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,22 @@ class UsersController < ApplicationController
         render json: users 
     end 
 
-    
+    def show 
+        user = User.find_by(id: params[:id])
+        render json: user
+    end 
+
+    def create 
+        user = Post.new(user_params)
+        if post.save 
+            render json: user
+        else 
+            render json: user.errors, status: :unprocessable_entity
+        end 
+    end 
+
+
+
 
     def user_params
         params.require(params.require(:user).permit(:name, :account_balance)
