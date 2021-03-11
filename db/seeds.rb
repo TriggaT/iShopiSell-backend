7b7fa6cb22729730c1a3fd25bc7a6611ffd6d64c
Trayshawn <traytwebb14@yahoo.com>
2021-03-10 20:17:21 -0600

    added an admin as user to collect funds on products in market

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+admin = User.create(name:"adminBank", account_balance: "200")
 jeff = User.create(name: "Jeff", account_balance: "1000") 
 neisha = User.create(name: "Neisha", account_balance: "500")
 trey = User.create(name: "Trey", account_balance: "200")
 
-music = Product.create(name: "Music CDs", price: 15, quantity: 50, user_id: 3)
-personal_assistant = Product.create(name: "Alexa", price: 50, quantity: 30, user_id:1 )
-makeup = Product.create(name: "Make-Up Kit", price: 25, quantity: 20, user_id: 2)
-boardwalk = Product.create(name: "Boardwalk", price: 400, quantity: 10, user_id: 1) 
-beat_machine = Product.create(name: "808 Beat Machine", price: 120, quantity: 25, user_id: 3) 
-phone = Product.create(name: "Limited Edition iPhone", price: 200, quantity: 1, user_id: 1) 
-book = Product.create(name: "The Best Thing Yet", price: 20, quantity: 30, user_id: 2)
+music = Product.create(name: "Music CDs", price: 15, quantity: 50, user_id: 4)
+personal_assistant = Product.create(name: "Alexa", price: 50, quantity: 30, user_id:2 )
+makeup = Product.create(name: "Make-Up Kit", price: 25, quantity: 20, user_id: 3)
+boardwalk = Product.create(name: "Boardwalk", price: 400, quantity: 10, user_id: 2) 
+beat_machine = Product.create(name: "808 Beat Machine", price: 120, quantity: 25, user_id: 4) 
+phone = Product.create(name: "Limited Edition iPhone", price: 200, quantity: 1, user_id: 2) 
+book = Product.create(name: "The Best Thing Yet", price: 20, quantity: 30, user_id: 3)
 
