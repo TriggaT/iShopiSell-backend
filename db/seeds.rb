dc177d4a0e3cc5fc445f8ac6109266342fdf61f8
Trayshawn <traytwebb14@yahoo.com>
2021-03-12 14:01:09 -0600

    changed name of product in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,5 @@ makeup = Product.create(name: "Make-Up Kit", price: 25, quantity: 20, user_id: 3
 boardwalk = Product.create(name: "Boardwalk", price: 400, quantity: 10, user_id: 2) 
 beat_machine = Product.create(name: "808 Beat Machine", price: 120, quantity: 25, user_id: 4) 
 phone = Product.create(name: "Limited Edition iPhone", price: 200, quantity: 1, user_id: 2) 
-book = Product.create(name: "The Best Thing Yet", price: 20, quantity: 30, user_id: 3)
+book = Product.create(name: "The Best Thing Yet Book", price: 20, quantity: 30, user_id: 3)
 
